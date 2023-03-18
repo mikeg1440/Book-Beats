e68ae7972809461b5e9827682047dbc578ee4e3c
Mike Gaudreau <gaudreaum01@gmail.com>
2023-03-18 16:27:19 -0400

    Create a file with the saved youtube search results data for mock requests

---
+++ pages/data/youtube.js
@@ -0,0 +1,412 @@
+const youtubeData = [
+  {
+    position_on_page: 1,
+    title: "Kansas - Carry On Wayward Son (Official Audio)",
+    link: "https://www.youtube.com/watch?v=2X_2IdybTV0",
+    channel: {
+      name: "KANSAS",
+      link: "https://www.youtube.com/channel/UC2wAI2YT-k29MpB3JZ8NEfQ",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/AOJE5u1NfmBk096_FmT5ozMWLsRvTvTc5EwZnCjJkfST8TEftwaqGwk-0vDmO54-9oHQ0kdw7Q=s88-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "10 years ago",
+    views: 197680991,
+    length: "5:24",
+    description:
+      "Lyrics: Carry on, my wayward son There'll be peace when you are done Lay your weary head to rest Don't you cry no more ...",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/2X_2IdybTV0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBi5ljdjbeTdQX9CiJLyA0UV_bjlA",
+      rich: "https://i.ytimg.com/an_webp/2X_2IdybTV0/mqdefault_6s.webp?du=3000&sqp=CJuI2KAG&rs=AOn4CLDI-wXZ9_8Sy_uqEMD6jfoC8KeZ8A",
+    },
+  },
+  {
+    position_on_page: 2,
+    title: "Kansas - Carry on Wayward Son (Official Video)",
+    link: "https://www.youtube.com/watch?v=P5ZJui3aPoQ",
+    channel: {
+      name: "KANSAS",
+      link: "https://www.youtube.com/channel/UC2wAI2YT-k29MpB3JZ8NEfQ",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/AOJE5u1NfmBk096_FmT5ozMWLsRvTvTc5EwZnCjJkfST8TEftwaqGwk-0vDmO54-9oHQ0kdw7Q=s88-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "6 years ago",
+    views: 76889153,
+    length: "5:24",
+    description:
+      "Chorus: Carry on, my wayward son There'll be peace when you are done Lay your weary head to rest Don't you cry no more ...",
+    extensions: ["CC"],
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/P5ZJui3aPoQ/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDZMxP0HmdkSC_xWAf7ALQkH5Up4w",
+      rich: "https://i.ytimg.com/an_webp/P5ZJui3aPoQ/mqdefault_6s.webp?du=3000&sqp=CKDY16AG&rs=AOn4CLDcXSPth9fmTLzdwTHgkwHhjU6leA",
+    },
+  },
+  {
+    position_on_page: 3,
+    title:
+      "Vocal Coach reacts to Kansas - Carry On Wayward Son HD (Live - 2009)",
+    link: "https://www.youtube.com/watch?v=t-trRCTt_Zg",
+    channel: {
+      name: "Beth Roars",
+      link: "https://www.youtube.com/@BethRoars",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/ytc/AL5GRJUH6xILviGwTmGTjse3wsDot97VR3HxOtjt2Eg3-g=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "1 hour ago",
+    views: 2041,
+    length: "11:03",
+    description:
+      "Want to make a reaction suggestion? Text me with the songs name and artist here: +1817-290-6610 ** Check out my second ...",
+    extensions: ["New", "4K"],
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/t-trRCTt_Zg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4BdLy5XHef5AvZmtle9NurawPVA",
+      rich: "https://i.ytimg.com/an_webp/t-trRCTt_Zg/mqdefault_6s.webp?du=3000&sqp=CLDS16AG&rs=AOn4CLAPHg4EWa3Y1OKPt9NVCiAagbILnQ",
+    },
+  },
+  {
+    position_on_page: 4,
+    title: "Kansas - Carry On Wayward Son HD (Live - 2009)",
+    link: "https://www.youtube.com/watch?v=PVH-u9gIy8s",
+    channel: {
+      name: "Kingscup20",
+      link: "https://www.youtube.com/@Kingscup20",
+      thumbnail:
+        "https://yt3.ggpht.com/ytc/AL5GRJWvpmyhxTTH1QyuEwAal4a0AYTmRwA47eKwxg=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "13 years ago",
+    views: 2545456,
+    length: "6:43",
+    description: "Kansas - Carry On Wayward Son HD (Live - 2009)",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/PVH-u9gIy8s/hq720.jpg?sqp=-oaymwE2COgCEMoBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARh_IFcoEzAP&rs=AOn4CLAMB4Mg8dnj26484KzNhHFnn11S3A",
+      rich: "https://i.ytimg.com/an_webp/PVH-u9gIy8s/mqdefault_6s.webp?du=3000&sqp=CPKQ2KAG&rs=AOn4CLBpW1tB3-Rd7vm9_j5EvRPKBrYk5g",
+    },
+  },
+  {
+    position_on_page: 5,
+    title: "Kansas - Carry On Wayward Son (Live from Canada Jam)",
+    link: "https://www.youtube.com/watch?v=s38ignmTqFQ",
+    channel: {
+      name: "KANSAS",
+      link: "https://www.youtube.com/channel/UC2wAI2YT-k29MpB3JZ8NEfQ",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/AOJE5u1NfmBk096_FmT5ozMWLsRvTvTc5EwZnCjJkfST8TEftwaqGwk-0vDmO54-9oHQ0kdw7Q=s88-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "7 years ago",
+    views: 2627213,
+    length: "4:27",
+    description:
+      "Lyrics: Carry on, my wayward son There'll be peace when you are done Lay your weary head to rest Don't you cry no more ...",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/s38ignmTqFQ/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDnCIatoRmseQdfmI2uAMU_ZUEEgQ",
+      rich: "https://i.ytimg.com/an_webp/s38ignmTqFQ/mqdefault_6s.webp?du=3000&sqp=CLD216AG&rs=AOn4CLBi1FMGtvUeZ3Q9yrMOG8C7HuatQw",
+    },
+  },
+  {
+    position_on_page: 6,
+    title: "Kansas-Carry On Wayward Son HQ",
+    link: "https://www.youtube.com/watch?v=JtlzlGVelfo",
+    channel: {
+      name: "Mohd Zakaria",
+      link: "https://www.youtube.com/@MrZint18",
+      thumbnail:
+        "https://yt3.ggpht.com/ytc/AL5GRJWh93uhRN_KGXZg_i938Yk8qe8HEfXNYLEod-0G=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "11 years ago",
+    views: 191504,
+    length: "5:23",
+    description: "supernatural Theme Song.",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/JtlzlGVelfo/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAbbG2OZp-3ck_LlhgK_PMVn6Fwvg",
+    },
+  },
+  {
+    position_on_page: 9,
+    title: "Carry On Wayward Son",
+    link: "https://www.youtube.com/watch?v=aVAsZmFICQk",
+    channel: {
+      name: "KANSAS",
+      link: "https://www.youtube.com/channel/UC2wAI2YT-k29MpB3JZ8NEfQ",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/AOJE5u1NfmBk096_FmT5ozMWLsRvTvTc5EwZnCjJkfST8TEftwaqGwk-0vDmO54-9oHQ0kdw7Q=s88-c-k-c0x00ffffff-no-rj",
+    },
+    views: 281176,
+    length: "3:37",
+    description:
+      "Provided to YouTube by SBME Strategic Marketing Group Carry On Wayward Son · Kansas Rock: The Train Kept A Rollin' ℗ 1976 ...",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/aVAsZmFICQk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAIcuH5eWx-6z2G0Iq9_jYT9uzE0Q",
+    },
+  },
+  {
+    position_on_page: 10,
+    title:
+      'School of Rock AllStar Students perform a cover of "Carry On Wayward Son" by Kansas',
+    link: "https://www.youtube.com/watch?v=yRneFCTuiuQ",
+    channel: {
+      name: "School of Rock",
+      link: "https://www.youtube.com/@SchoolofRockUSA",
+      thumbnail:
+        "https://yt3.ggpht.com/ytc/AL5GRJXT20yBeNc-vhljKXl_Qry1BAZVq7E25VX1KeEYWA=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "11 months ago",
+    views: 712362,
+    length: "5:39",
+    description:
+      'School of Rock 2021 AllStar Students perform a cover of "Carry On Wayward Son" by Kansas at Rax Trax Recording in Chicago.',
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/yRneFCTuiuQ/hq720.jpg?sqp=-oaymwE2COgCEMoBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARh_IEAoGjAP&rs=AOn4CLC9VvpzceROeQFzK0IJ515ZYSX5NA",
+      rich: "https://i.ytimg.com/an_webp/yRneFCTuiuQ/mqdefault_6s.webp?du=3000&sqp=CLTQ16AG&rs=AOn4CLDyN-QUFTy3HDPZF8NKF4GZH0k_9A",
+    },
+  },
+  {
+    position_on_page: 11,
+    title:
+      "Kansas - Carry On Wayward Son (Bass Cover) (Play Along Tabs In Video)",
+    link: "https://www.youtube.com/watch?v=RsdbZrmjJjE",
+    channel: {
+      name: "CoverSolutions",
+      link: "https://www.youtube.com/@CoverSolutions",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/ytc/AL5GRJUmj3IXV3HyvPIp0qgmGnH5dYjtLD2BOJjgODKa=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "6 years ago",
+    views: 970874,
+    length: "5:45",
+    description:
+      "Tips And Comments: - Playing Technique: Played between pickups with different dynamic through the song (verses a little less ...",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/RsdbZrmjJjE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5Q_hix3AQNojBnbeYr-mqlLGG7Q",
+      rich: "https://i.ytimg.com/an_webp/RsdbZrmjJjE/mqdefault_6s.webp?du=3000&sqp=COn_16AG&rs=AOn4CLBt77756FbJDnIiBeBDiPQXivcxuA",
+    },
+  },
+  {
+    position_on_page: 12,
+    title: "Kansas - Carry On Wayward Son (single version) (1977)",
+    link: "https://www.youtube.com/watch?v=_CzHj_YdyFI",
+    channel: {
+      name: "menchitty11",
+      link: "https://www.youtube.com/@menchitty11",
+      thumbnail:
+        "https://yt3.ggpht.com/ytc/AL5GRJVa6Yg_7HvLHM3X-PHo-NvbCK2FNXxU3Gno-ah3=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "2 years ago",
+    views: 21078,
+    length: "3:30",
+    description:
+      'The first single for Kansas to make the Top 20! Recorded in 1976 but charted a year later, "Carry On Wayward Son" missed the ...',
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/_CzHj_YdyFI/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA7XwLYPldBQ7_RwvzhxmLMeVhpoQ",
+      rich: "https://i.ytimg.com/an_webp/_CzHj_YdyFI/mqdefault_6s.webp?du=3000&sqp=CJb116AG&rs=AOn4CLByimZGb5qBDhOBBWB6Uh4fE7HcYw",
+    },
+  },
+  {
+    position_on_page: 13,
+    title: "Kansas Carry On My Wayward Son 1 Hour [Supernatural Theme Song]",
+    link: "https://www.youtube.com/watch?v=GVoikA7CjaQ",
+    channel: {
+      name: "KB Vegas",
+      link: "https://www.youtube.com/@KBVegas",
+      thumbnail:
+        "https://yt3.ggpht.com/9d-W_OSpcF5Zz5cygDWrrhjq9FnX9BkVT9abLyOcDOJpKmZYGf6Gs23Cu1uqhyvQmiJwZP70fAI=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "3 years ago",
+    views: 93966,
+    length: "1:03:14",
+    description:
+      "Thanks so much for listening/watching my new hour long song Carry On My Wayward Son the song is also the theme song for ...",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/GVoikA7CjaQ/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLClEvn-03_BoZbKzu0kSINy6KDylQ",
+    },
+  },
+  {
+    position_on_page: 14,
+    title: "Kansas - Dust in the Wind (Official Video)",
+    link: "https://www.youtube.com/watch?v=tH2w6Oxx0kQ",
+    channel: {
+      name: "KANSAS",
+      link: "https://www.youtube.com/channel/UC2wAI2YT-k29MpB3JZ8NEfQ",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/AOJE5u1NfmBk096_FmT5ozMWLsRvTvTc5EwZnCjJkfST8TEftwaqGwk-0vDmO54-9oHQ0kdw7Q=s88-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "13 years ago",
+    views: 226171949,
+    length: "3:20",
+    description:
+      "Lyrics: Dust in the wind All they are is dust in the wind #Kansas #DustintheWind #Rock.",
+    extensions: ["CC"],
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/tH2w6Oxx0kQ/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD57MncS1duBFLosnM0LbdXgpk16Q",
+      rich: "https://i.ytimg.com/an_webp/tH2w6Oxx0kQ/mqdefault_6s.webp?du=3000&sqp=CPjs16AG&rs=AOn4CLBaFpQVtYo-dbu13UxDyTxgvYLI7g",
+    },
+  },
+  {
+    position_on_page: 16,
+    title:
+      "Kansas - Carry On Wayward Son Guitar Lesson Pt.1 - Main Riffs & Intro Solo",
+    link: "https://www.youtube.com/watch?v=DSgH7IWmNrE",
+    channel: {
+      name: "GuitarLessons365Song",
+      link: "https://www.youtube.com/@GuitarLessons365SongLessons",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/zspxfSxSPVyhTzLz42CDbfvjawo8FYsG2galZyRiy1Y6_twsB85k2EW8lrsPpF46FssPIa0nZfk=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "11 years ago",
+    views: 779262,
+    length: "14:10",
+    description:
+      'In this video guitar lesson series you will learn how to play the classic Kansas arena rock masterpiece "Carry On My Wayward ...',
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/DSgH7IWmNrE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDaYpR0PxWpeaJXRfhRWRiIf6Fsrw",
+      rich: "https://i.ytimg.com/an_webp/DSgH7IWmNrE/mqdefault_6s.webp?du=3000&sqp=CKOK2KAG&rs=AOn4CLC21xWYFQ3biIqP3cl_D1Exjo82DA",
+    },
+  },
+  {
+    position_on_page: 17,
+    title: "Kansas - Carry On Wayward Son (Live 1982)",
+    link: "https://www.youtube.com/watch?v=NfXvWKQn03E",
+    channel: {
+      name: "ArizonaBasement",
+      link: "https://www.youtube.com/@ArizonaBasement",
+      thumbnail:
+        "https://yt3.ggpht.com/ytc/AL5GRJWFPlMG5HKP1V5VVPr9x7KmGtp9NBKKoLBDqYq6=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "14 years ago",
+    views: 459848,
+    length: "6:18",
+    description:
+      'Live performance of "Carry On Wayward Son" performed by Kansas in Omaha, NE in July 1982. This song is from their ...',
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/NfXvWKQn03E/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBazPkZUnG7NA2o7rdusBleQizW7g",
+      rich: "https://i.ytimg.com/an_webp/NfXvWKQn03E/mqdefault_6s.webp?du=3000&sqp=CMjn16AG&rs=AOn4CLBO0Wej59X94AxxZ_44bosS4C_Srw",
+    },
+  },
+  {
+    position_on_page: 18,
+    title: '"Carry On Wayward Son" - Kansas (Cover by First to Eleven)',
+    link: "https://www.youtube.com/watch?v=iy76QRhYY8k",
+    channel: {
+      name: "First To Eleven",
+      link: "https://www.youtube.com/channel/UCcE10s4MFy4eed7q7QkonZg",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/Xsyf4kJrHNMZl8ZJD-2wSWe_1Ob3s2hNFjQj0QCaJZjBXUZ40ORfoWgIilhxLEz_XR9NM7ogbg=s88-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "1 year ago",
+    views: 1761907,
+    length: "5:44",
+    description: "first2eleven@gmail.com #FirsttoEleven #Cover #Rock #Pop.",
+    extensions: ["4K"],
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/iy76QRhYY8k/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCz8fVJOLYJh1XhB0iRXzEw_7o22g",
+      rich: "https://i.ytimg.com/an_webp/iy76QRhYY8k/mqdefault_6s.webp?du=3000&sqp=CLH716AG&rs=AOn4CLDBNKAXLn6wDllRSviPnILXip-DsQ",
+    },
+  },
+  {
+    position_on_page: 20,
+    title: "Kansas - Carry On Wayward Son [1 Hour]",
+    link: "https://www.youtube.com/watch?v=TJjyK1Al6Uk",
+    channel: {
+      name: "1HM",
+      link: "https://www.youtube.com/@1HMOneHourMusic",
+      thumbnail:
+        "https://yt3.ggpht.com/ytc/AL5GRJWWWFMAbSrnO-KAsvgoMasbRbQoHBn5Cg6qnh2p=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "2 years ago",
+    views: 28499,
+    length: "1:04:40",
+    description:
+      "Hey guys! its been a while since I last did a loop! But Enjoy! I DO NOT OWN THIS SONG! Original link ...",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/TJjyK1Al6Uk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA89qrSbATwaerfqgJc2jTDSnUQjQ",
+    },
+  },
+  {
+    position_on_page: 23,
+    title: "Carry On My Wayward Son Guitar Backing Track",
+    link: "https://www.youtube.com/watch?v=eWRDjZq_92M",
+    channel: {
+      name: "Revin Enriquez",
+      link: "https://www.youtube.com/@revinenriquez9941",
+      thumbnail:
+        "https://yt3.ggpht.com/ytc/AL5GRJXWG60lXhYf12goCSK-RS5w8qqh53OJqjwnJw=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "5 years ago",
+    views: 98981,
+    length: "5:25",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/eWRDjZq_92M/hqdefault.jpg?sqp=-oaymwE2COADEI4CSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB3gGAAuABigIMCAAQARhhIGEoYTAP&rs=AOn4CLAzY7hEFiumCOMCK7oPvRpg3V8HAQ",
+    },
+  },
+  {
+    position_on_page: 24,
+    title: "Neoni - Carry On Wayward Son (Official Music Video)",
+    link: "https://www.youtube.com/watch?v=qKvj12YTtI4",
+    channel: {
+      name: "Neoni",
+      link: "https://www.youtube.com/channel/UCQJ-a2IzCJ-gwlHvqvOWGhw",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/8z_QTpi5SDg1wn2cE_P-VmsREtAt3wZY2ymc8qkLx5RL3q-Hryv_mOml-EmMmxeSvXnFBmtE=s88-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "4 years ago",
+    views: 6229725,
+    length: "3:28",
+    description:
+      "#neoni #carryonwaywardson #darkside ────────── ✘ LYRICS ✘ ─────────── … Once I rose above the ...",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/qKvj12YTtI4/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCIgOK3R9-z3CAn406wX3wcN7eBnw",
+      rich: "https://i.ytimg.com/an_webp/qKvj12YTtI4/mqdefault_6s.webp?du=3000&sqp=CO7X16AG&rs=AOn4CLAbFjHQFZ4icX6l2SCrPpTX269b0Q",
+    },
+  },
+  {
+    position_on_page: 25,
+    title:
+      "Carry On Wayward Son (metal cover by Leo Moracchioli feat. Truls Haugen)",
+    link: "https://www.youtube.com/watch?v=5Sw3CXk3TvE",
+    channel: {
+      name: "Frog Leap Studios",
+      link: "https://www.youtube.com/@leolego",
+      verified: true,
+      thumbnail:
+        "https://yt3.ggpht.com/ytc/AL5GRJVMx23IHJ-MC2A1iQWzhmRxUBSWsYGaXgpOjT5wTA=s68-c-k-c0x00ffffff-no-rj",
+    },
+    published_date: "3 years ago",
+    views: 5324932,
+    length: "8:45",
+    description:
+      "Original by Kansas Check out Truls channel here: https://www.youtube.com/user/TrulsMaximus/videos You can buy my albums or ...",
+    thumbnail: {
+      static:
+        "https://i.ytimg.com/vi/5Sw3CXk3TvE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAR7iu1TG-YFbzUyc0XcP_OPKEKBA",
+      rich: "https://i.ytimg.com/an_webp/5Sw3CXk3TvE/mqdefault_6s.webp?du=3000&sqp=CLvv16AG&rs=AOn4CLB2PZ1UVO2xnRArNfX4DBeWSDIJPg",
+    },
+  },
+];
+
+export default youtubeData;
