831bfa54579068762d8b681799e85946e9bc1660
Mike Gaudreau <gaudreaum01@gmail.com>
2023-03-18 16:27:47 -0400

    Create a file to hold the data for mock open ai requests

---
+++ pages/data/openAi.js
@@ -0,0 +1,8 @@
+const openAiData = {
+  text: " and the Sorcerer's Stone by J.K. Rowling\n\n\"Hedwig's Theme - John Williams, Accio! - The Remus Lupins, Mischief Managed - John Williams, Hogwarts' Hymn - John Williams, Platform 9 3/4 - John Williams, Hogwarts School March - John Williams",
+  index: 0,
+  logprobs: null,
+  finish_reason: "stop",
+};
+
+export default openAiData;
