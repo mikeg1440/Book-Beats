fe4ffb6bb27c0913f8d50b1ddac3ba4b6b73a8e4
Mike Gaudreau <gaudreaum01@gmail.com>
2023-03-23 20:07:51 -0400

    Refactor code to use objects instead of strings for videos and add loading component

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,38 +4,43 @@ import { useState, useRef } from "react";
 import SearchBar from "./components/SearchBar";
 import VideoPlayer from "./components/VideoPlayer";
 import Playlist from "./components/Playlist";
+import Videolist from "./components/Videolist";
+import Loading from "./components/Loading";
 
 const App = () => {
-  const [currentSong, setCurrentSong] = useState("");
-  const [songs, setSongs] = useState([]);
-  const [videoSrc, setVideoSrc] = useState("");
+  const [currentVideo, setCurrentVideo] = useState({});
+  const [playlist, setPlaylist] = useState([]);
+  const [videos, setVideos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  // parse data that returns from open ai and save to state
   const parseData = (data) => {
     let [title, songString] = data.split("\n\n");
     songString = songString.replace(/"/g, "");
     const songs = songString.split(", ");
-    // setCurrentSong(songs[0]);
-    setSongs(songs);
-    getYoutubeVideo(currentSong);
+    // setCurrentVideo(songs[0]);
+    setPlaylist(songs);
+    setCurrentVideo(songs[0]);
+    getYoutubeVideos(currentVideo.title);
   };
 
   // transforms a youtube url link to a embed link that works with the iframe
   const transformYoutubeUrl = (originalUrl) => {
-    const regX = /v=([\w]+)/gi;
+    const regX = /v=([\w\W]+)/gi;
     const regMatch = originalUrl.match(regX);
 
     const embedId = regMatch[0].replace("v=", "");
 
     if (!embedId.length > 0) {
       console.log(embedId);
-      // throw new Error("Embed ID not found!");
       throw new Error("Couldn't get embed ID from youtube video!");
     }
 
     return `https://www.youtube.com/embed/${embedId}?&autoplay=1`;
   };
 
-  const getYoutubeVideo = (searchTerm) => {
+  // searches for youtube videos using the song suggestion from open ai and saves the top 5 videos to videos state
+  const getYoutubeVideos = (searchTerm) => {
     // get list of results from a youtube search using fetch request
     const prodApi = "/api/youtubeSearch";
     const devApi = "/api/mockYoutube";
@@ -46,14 +51,14 @@ const App = () => {
     })
       .then((resp) => resp.json())
       .then((resp) => {
-        const embedUrl =
-          typeof resp === "string"
-            ? transformYoutubeUrl(JSON.parse(resp)[0].link)
-            : transformYoutubeUrl(resp[0].link);
-        typeof resp === "string"
-          ? setCurrentSong(JSON.parse(resp)[0].title)
-          : setCurrentSong(resp[0].title);
-        setVideoSrc(embedUrl);
+        const vids = typeof resp === "string" ? JSON.parse(resp) : resp;
+
+        const embedUrl = transformYoutubeUrl(vids[0].link);
+
+        vids[0].embedUrl = embedUrl;
+        setCurrentVideo(vids[0]);
+        setVideos(vids.slice(0, 5));
+        setIsLoading(false);
       })
       .catch((err) => console.log("Failed to get youtube data", err));
   };
@@ -61,6 +66,9 @@ const App = () => {
   const handleGenerate = async (bookTitle) => {
     const prodApi = "/api/generate";
     const devApi = "/api/mockAI";
+
+    setIsLoading(true);
+
     fetch(devApi, {
       method: "POST",
       headers: {
@@ -71,22 +79,33 @@ const App = () => {
       .then((resp) => resp.json())
       .then((resp) => {
         const output = resp.output;
-        console.log(output);
         parseData(output.text);
       })
       .catch((err) => console.log("Failed to get ai output", err));
   };
 
   const newSongSelected = (song) => {
-    alert(`New song selected!\n${song}`);
+    getYoutubeVideos(song);
+  };
+
+  const newVideoSelected = (video) => {
+    if (!video.embedUrl) {
+      video.embedUrl = transformYoutubeUrl(video.link);
+    }
+    setCurrentVideo(video);
   };
 
   const togglePlaylist = () => {
     const playlist = document.querySelector("#playlist");
-    console.log("songs", songs);
     if (playlist) {
       playlist.style.display = "grid";
-      // playlist.style.display = "flex";
+    }
+  };
+
+  const toggleVideolist = () => {
+    const videolist = document.querySelector("#videolist");
+    if (videolist) {
+      videolist.style.display = "grid";
     }
   };
 
@@ -98,14 +117,21 @@ const App = () => {
         <SearchBar handleGenerate={handleGenerate} />
       </AppHeader>
 
+      <ToggleContainer>
+        <Playlist list={playlist} newSelectionCallback={newSongSelected} />
+        <PlaylistButton onClick={togglePlaylist}>Open Playlist</PlaylistButton>
+        <Videolist list={videos} newSelectionCallback={newVideoSelected} />
+        <PlaylistButton onClick={toggleVideolist}>
+          Open Videolist
+        </PlaylistButton>
+      </ToggleContainer>
+
       <LeftImage src="/equilizer.png" />
 
-      <VideoPlayer currentSong={currentSong} videoSrc={videoSrc} />
+      <Loading isLoading={isLoading} />
+      <VideoPlayer currentVideo={currentVideo} />
 
       <RightImage src="/music_notes.png" />
-
-      <Playlist songs={songs} newSongSelected={newSongSelected} />
-      <PlaylistButton onClick={togglePlaylist}>Open Playlist</PlaylistButton>
     </AppContainer>
   );
 };
@@ -165,4 +191,10 @@ const PlaylistButton = styled.div`
     background-color: orange;
     color: white;
   }
+`;
+
+const ToggleContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 50%;
 `;
